d60eb5257efa8be543de052b3acd2cac56cb6248
Shruthi1412 <47416139+Shruthi1412@users.noreply.github.com>
2019-05-25 19:17:20 +0530

    Update adminlog.php

--- adminlog.php
+++ adminlog.php
@@ -5,9 +5,9 @@ $password = $_POST['password'];
 
 require_once('dbHelper1.php');
 $sql = "SELECT * FROM data WHERE email='$username' and password='$password' and designation='Manager'";
-$res = mysql_query($con,$sql);
-$check = mysql_fetch_array($res);
-if(isset($check)){
+$res = mysqli_query($con,$sql);
+$check = mysqli_fetch_array($res);
+if($check){
 echo 'success';
 }else{
 echo 'failure';
