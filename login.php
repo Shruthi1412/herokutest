072524c57ce4f20515efdd8513860761647c0403
Shruthi1412 <47416139+Shruthi1412@users.noreply.github.com>
2019-05-25 18:32:04 +0530

    Update login.php

--- login.php
+++ login.php
@@ -9,7 +9,7 @@ if(isset($_POST['btnSubmit']))
     if($res=mysql_fetch_array($result))
     {
         echo "<script>alert(\"Login Successful\");</script>";
-        header("location: admin.php");
+        header("location: https://leavemanagementheroku.herokuapp.com/admin.php");
         
         
     }
