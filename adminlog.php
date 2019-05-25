be324e80419da7429d3fa499adfd20f6a5e36471
Shruthi1412 <47416139+Shruthi1412@users.noreply.github.com>
2019-05-25 18:59:00 +0530

    Update adminlog.php

--- adminlog.php
+++ adminlog.php
@@ -4,7 +4,7 @@ $username = $_POST['username'];
 $password = $_POST['password'];
 
 require_once('dbHelper1.php');
-$sql = "SELECT * FROM data WHERE email='$username' and password='$password' and designation='Manager'";
+$sql = "select * from data where email='$username' and password='$password' and designation='Manager'";
 $res = mysqli_query($con,$sql);
 $check = mysqli_fetch_array($res);
 if(isset($check)){
